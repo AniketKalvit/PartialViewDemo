3dbbc0a200c31866bbe124a9a0962e49349a3952
aniketkalvit <aniketkalvit@gmail.com>
2023-10-03 22:01:07 +0530

    Add project files.

---
+++ PartialViewDemo/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewDemo.Models;
+
+namespace PartialViewDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private ProductCRUD db;
+        public ProductController(ApplicationDbContext context)
+        {
+            this.context = context;
+            db = new ProductCRUD(this.context);
+        }
+        public IActionResult Index()
+        {
+            return View(db.GetProducts());
+        }
+    }
+}
---
+++ PartialViewDemo/Models/ProductCRUD.cs
@@ -0,0 +1,15 @@
+﻿namespace PartialViewDemo.Models
+{
+    public class ProductCRUD
+    {
+        private readonly ApplicationDbContext db;
+        public ProductCRUD(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            return db.Products.ToList();
+        }
+    }
+}
